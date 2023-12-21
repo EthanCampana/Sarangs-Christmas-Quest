51efec23fe3a3e28c8bdb3e0b44f5141255c8dba
Allan Raposo <araposo1@jh.edu>
2023-12-20 22:04:23 -0500

    implemented transition

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,26 +1,26 @@
 extends Control
+
+@export var next_scene: PackedScene
 @onready var animation_player = $MarginContainer/VBoxContainer/AnimationPlayer
 
 var scene_changing = false
+
 func _ready():
 	set_process_input(true)
 	start_fading_animation()
-	
 	
 func _input(event):
 	if event is InputEventKey and not scene_changing:
 		scene_changing = true
 	
-
 func start_fading_animation():
 	if animation_player.has_animation("fadeLoop"):
 		animation_player.play("fadeLoop")
 
-
-
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fadeLoop":
-		if scene_changing:
-			get_tree().change_scene_to_file("res://scenes/Level_1.tscn")
+		if scene_changing and next_scene != null:
+			SceneTransition.change_scene('res://level_creator.tscn')
 		else:
+			scene_changing = false
 			start_fading_animation()
