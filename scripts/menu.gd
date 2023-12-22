95c3edafbc3221be41131f906e136c0f0e91b843
Allan Raposo <araposo1@jh.edu>
2023-12-21 20:39:58 -0500

    Add assets

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -20,7 +20,7 @@ func start_fading_animation():
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fadeLoop":
 		if scene_changing and next_scene != null:
-			SceneTransition.change_scene('res://level_creator.tscn')
+			SceneTransition.change_scene('res://scenes/level_creator.tscn')
 		else:
 			scene_changing = false
 			start_fading_animation()
