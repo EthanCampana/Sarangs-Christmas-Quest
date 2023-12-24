f96e97e92fb57fd7c8733b9a7b815eaabcf7cb75
Ethan <ethancampana@gmail.com>
2023-12-24 00:54:17 -0500

    forest-level

--- scripts/Dead.gd
+++ scripts/Dead.gd
@@ -5,8 +5,19 @@ class_name Death
 # Defines what happens when the state is entered
 func Enter():
 	player.animation_player.current_animation = "Die"
+	player.audio.stream = player.cat_meow.pick_random()
+	player.audio.play()
 
+
+
+
+# Defines what happens when the state is updated every frame (Physics related)
+func Physics_Update(delta: float):
+	if not player.animation_player.is_playing():
+		state_machine.transition_to("idle")
 
 # Defines what happens when the state is exited
 func Exit():
-	pass
+	player.isDead = false
+	player.animation_player.play_backwards("Die")
+	await player.animation_player.animation_finished
--- scripts/Present.gd
+++ scripts/Present.gd
@@ -1,2 +1,6 @@
 extends Item
 class_name Present
+
+
+func _on_collectable_component_area_entered(area):
+	pass # Replace with function body.
